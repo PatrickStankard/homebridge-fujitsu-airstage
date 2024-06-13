bba13fb443eb459fa8187fe0e36e61fcbffa5183
Patrick Stankard <me@patrickstankard.com>
2024-06-12 21:54:05 -0400

    Add ConfigManager class to update Homebridge config

---
+++ src/config-manager.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const fs = require('node:fs');
+
+class ConfigManager {
+
+    constructor(config, api) {
+        this.api = api;
+        this.config = config;
+    }
+
+    updateConfigWithAccessToken(accessToken, accessTokenExpiry, refreshToken) {
+        const homebridgeConfigPath = this.api.user.configPath();
+        const homebridgeConfigString = this._readFileSync(homebridgeConfigPath);
+        let homebridgeConfig = JSON.parse(homebridgeConfigString);
+
+        this.config.email = null;
+        this.config.password = null;
+
+        this.config.accessToken = accessToken;
+        this.config.accessTokenExpiry = accessTokenExpiry.toISOString();
+        this.config.refreshToken = refreshToken;
+
+        homebridgeConfig.platforms.some(function(platformConfig, idx, platforms) {
+            if (platformConfig.platform === this.config.platform) {
+                platforms[idx] = this.config;
+
+                return true;
+            }
+        }, this);
+
+        const updatedConfigString = JSON.stringify(homebridgeConfig, null, 4);
+
+        if (updatedConfigString !== homebridgeConfigString) {
+            this._writeFileSync(homebridgeConfigPath, updatedConfigString);
+
+            return true;
+        }
+
+        return false;
+    }
+
+    _readFileSync(path) {
+        return fs.readFileSync(path).toString();
+    }
+
+    _writeFileSync(path, jsonString) {
+        return fs.writeFileSync(path, jsonString);
+    }
+}
+
+module.exports = ConfigManager;
